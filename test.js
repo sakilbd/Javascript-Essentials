eb3b73927be78725f1be4e9b63abd7aa4962c24a
Sakil <mdsakiluzzaman@gmail.com>
2022-06-08 11:59:50 +0600

    another problem solved

--- test.js
+++ test.js
@@ -1,22 +1,13 @@
-let maxProfit = function(prices) {
-    if (prices === []) {
-        return 0;
-    }
-    let profit = 0;
-    let i = 0;
-    while (i < prices.length - 1) {
-        let cp = prices[i];
-        let j = i + 1;
-        while (j < prices.length) {
-            let sp = prices[j]
-            profit = Math.max(profit, sp - cp);
-
-            j++;
+var maxDepth = function(root) {
+    count = 0;
+    for (let i = 0; i < Math.floor(root.length / 2); i++) {
+        if (root[i + 1] != 'null' & root[i + 2] != 'null') {
+            count++;
         }
-        i++;
+
+
     }
-    return profit;
+    return count;
 };
 
-
-console.log(maxProfit([7, 1, 5, 3, 6, 4]))
+console.log(maxDepth([3, 9, 20, null, null, 15, 7]));
