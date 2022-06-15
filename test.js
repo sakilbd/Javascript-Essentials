ed2dc8bf2b77481786c1bfa6e8d4671b39a4a6ae
Sakil <mdsakiluzzaman@gmail.com>
2022-06-15 18:48:30 +0600

    picking numbers problem added

--- test.js
+++ test.js
@@ -1,15 +1,44 @@
-const c = console.log.bind(console)
+// https://www.hackerrank.com/challenges/picking-numbers/problem?isFullScreen=true
+const c = console.log.bind(console);
 
-function pageCount(n, p) {
-    console.log(n);
-    console.log(p);
-    const fromBeginning = Math.floor(p / 2);
-    // c("fromStart :" + fromBeginning);
-    const fromEnd = Math.floor((n - p) / 2);
-    // c("fromEnd :" + fromEnd);
-    return fromBeginning < fromEnd ? fromBeginning : fromEnd;
-    // return Math.floor(p / 2) != 0 ? Math.floor(p / 2) : 1;
+function pickingNumbers(a) {
+    var arr = [...a];
+    const count = {};
+    for (let i = 0; i < arr.length; i++) {
+        count[arr[i]] = i;
+    }
 
+    const countLength = Object.keys(count).length;
+
+    const distinctElements = Object.keys(count).map((key) => {
+        count[key] = 0;
+        return key;
+    });
+    for (let k = 0; k < distinctElements.length; k++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (parseInt(distinctElements[k]) == parseInt(arr[j])) {
+                count[distinctElements[k]]++;
+            }
+        }
+    }
+    let objArray = [];
+    Object.keys(count).map((key, i) => {
+        objArray.push(key);
+    });
+    c(objArray);
+    let finalArray = [];
+    for (let i of objArray) {
+        for (let j = 1; j < objArray.length; j++) {
+            if (Math.abs(i - objArray[j]) <= 0) {
+                finalArray.push(i);
+                finalArray.push(objArray[j]);
+            } else {
+                objArray.splice(1);
+                c(objArray);
+            }
+        }
+    }
+    return finalArray;
 }
 
-c(pageCount(5, 4))
+c(pickingNumbers([4, 6, 5, 3, 3, 1]));
