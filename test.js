3986872b1c2e196a4bd5a7ff05f6489e0da4af8c
Sakil <mdsakiluzzaman@gmail.com>
2022-06-19 13:26:56 +0600

    some changes

--- test.js
+++ test.js
@@ -1,43 +1,16 @@
 const c = console.log.bind(console);
 
 
-const test = function(nums) {
+function angryProfessor(k, a) {
+    let present = 0;
+    let late = 0;
 
-    const numsMap = nums.map((item, i) => ({ item: item, index: i }))
-        // return numsMap[0];
-    return mergeSort(numsMap);
-
-
-}
-
-c(test([5, 4, 3, 2, 1]))
-
-
-function mergeSort(array) {
-
-    if (array.length <= 1) return array;
-    // return array;
-    const mid = Math.ceil((array.length) / 2) - 1;
-
-    // return mid;
-    let left = 0;
-    c(left);
-    left = mergeSort(array.slice(0, mid + 1));
-
-    let right = array.slice(mid + 1, array.length)
-        // return right;
-    c(left);
-
+    a.forEach((item) => {
+        item <= 0 ? present++ : late++;
+    })
+    return present >= k ? 'NO' : 'YES';
 
 }
 
 
-// function log(num) {
-//     // if (num > 15) {
-//     //     return;
-//     // }
-//     console.log(num);
-//     log(num - Math.random(10000000, 1000000000));
-// }
-// const num = Number.MAX_SAFE_INTEGER;
-// c(log(num));
+c(angryProfessor(4, [-2, -1, 0, 1, 2]))
