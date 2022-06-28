e32917c2adf65e19541847d102b142abca2633ba
Sakil <mdsakiluzzaman@gmail.com>
2022-06-28 19:10:08 +0600

    picking numbers added

--- pickingNumbers.js
+++ pickingNumbers.js
@@ -1,58 +1,52 @@
 // https://www.hackerrank.com/challenges/picking-numbers/problem?isFullScreen=true
+
+// accepted code by me
 const c = console.log.bind(console);
 
-
-//not solved
-function pickingNumbers(a) {
-    var arr = [...a];
-    const count = {};
-    for (let i = 0; i < arr.length; i++) {
-        count[arr[i]] = i;
-    }
-
-    const countLength = Object.keys(count).length;
-
-    const distinctElements = Object.keys(count).map((key) => {
-        count[key] = 0;
-        return key;
-    });
-    for (let k = 0; k < distinctElements.length; k++) {
-        for (let j = 0; j < arr.length; j++) {
-            if (parseInt(distinctElements[k]) == parseInt(arr[j])) {
-                count[distinctElements[k]]++;
-            }
-        }
-    }
-    let objArray = [];
-    Object.keys(count).map((key, i) => {
-        objArray.push(key);
-    });
-    c(objArray);
-    let finalArray = [];
-    for (let i of objArray) {
-        for (let j = 1; j < objArray.length; j++) {
-            if (Math.abs(i - objArray[j]) <= 0) {
-                finalArray.push(i);
-                finalArray.push(objArray[j]);
-            } else {
-                objArray.splice(1);
-                c(objArray);
-            }
-        }
-    }
-    return finalArray;
-}
-
-
-//optimal solution 
 // function pickingNumbers(a) {
-//     let maxcount = 0;
+//     const numbersCount = {};
+//     // let sorted = a.sort((a, b) => (a - b))
+//     a.forEach((item) => {
+//         numbersCount[item] = 0;
+//     });
 
-//     [...new Set(a)].forEach(x => {
-//         maxcount = Math.max(maxcount,
-//             a.reduce((c, v) => c += (v === x || v === x + 1), 0));
-//     })
-//     return maxcount;
+//     a.forEach((item) => {
+//         numbersCount[item] = numbersCount[item] + 1;
+//     });
+
+//     let finalArray = [];
+//     let highestOccurance = 0;
+//     Object.keys(numbersCount).map((item) => {
+//         //  numbersCount[item];
+//         if (highestOccurance < numbersCount[item]) {
+//             highestOccurance = numbersCount[item];
+//         }
+//         // c(!numbersCount[parseInt(item) + 1]);
+//         if (!numbersCount[parseInt(item) + 1] == false) {
+//             finalArray.push(counter(item, parseInt(item) + 1, numbersCount));
+//             // c("false");
+//         }
+//         // c(numbersCount[item]);
+//     });
+
+//     let result = finalArray.sort((a, b) => a - b);
+
+//     return result[result.length - 1] > highestOccurance ?
+//         result[result.length - 1] :
+//         highestOccurance;
+//     // return highestOccurance;
 // }
 
-c(pickingNumbers([4, 6, 5, 3, 3, 1]));
+// function counter(a, b, obj) {
+//     return parseInt(obj[b]) + parseInt(obj[a]);
+// }
+
+function pickingNumbers(a) {
+    let maxcount = 0;
+    [...new Set(a)].forEach(x => {
+        maxcount = Math.max(maxcount,
+            a.reduce((c, v) => c += (v === x || v === x + 1), 0));
+    })
+    return maxcount;
+}
+c(pickingNumbers([6, 6, 6, 6, 6, 6, 1, 2, 3, 3, 3, 3, 3, 3]));
--- test.js
+++ test.js
@@ -1,7 +1,52 @@
+// https://www.hackerrank.com/challenges/picking-numbers/problem?isFullScreen=true
+
+// accepted code by me
 const c = console.log.bind(console);
 
+// function pickingNumbers(a) {
+//     const numbersCount = {};
+//     // let sorted = a.sort((a, b) => (a - b))
+//     a.forEach((item) => {
+//         numbersCount[item] = 0;
+//     });
+
+//     a.forEach((item) => {
+//         numbersCount[item] = numbersCount[item] + 1;
+//     });
+
+//     let finalArray = [];
+//     let highestOccurance = 0;
+//     Object.keys(numbersCount).map((item) => {
+//         //  numbersCount[item];
+//         if (highestOccurance < numbersCount[item]) {
+//             highestOccurance = numbersCount[item];
+//         }
+//         // c(!numbersCount[parseInt(item) + 1]);
+//         if (!numbersCount[parseInt(item) + 1] == false) {
+//             finalArray.push(counter(item, parseInt(item) + 1, numbersCount));
+//             // c("false");
+//         }
+//         // c(numbersCount[item]);
+//     });
+
+//     let result = finalArray.sort((a, b) => a - b);
+
+//     return result[result.length - 1] > highestOccurance ?
+//         result[result.length - 1] :
+//         highestOccurance;
+//     // return highestOccurance;
+// }
+
+// function counter(a, b, obj) {
+//     return parseInt(obj[b]) + parseInt(obj[a]);
+// }
 
 function pickingNumbers(a) {
-    console.log(a)
-
+    let maxcount = 0;
+    [...new Set(a)].forEach(x => {
+        maxcount = Math.max(maxcount,
+            a.reduce((c, v) => c += (v === x || v === x + 1), 0));
+    })
+    return maxcount;
 }
+c(pickingNumbers([6, 6, 6, 6, 6, 6, 1, 2, 3, 3, 3, 3, 3, 3]));
