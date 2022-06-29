d29354754ab8778f5d31e98b7ce0627aef6ed607
Sakil <mdsakiluzzaman@gmail.com>
2022-06-29 18:13:26 +0600

    some changes

---
+++ permutations.js
@@ -0,0 +1,31 @@
+// https://www.hackerrank.com/challenges/cards-permutation/problem
+const c = console.log.bind(console);
+
+
+
+function solve(x) {
+    const set = [...new Set(x)];
+    if (x.length - set.length == 1) {
+        return permutations(x).length - 1;
+    } else {
+        return permutations(x).length;
+    }
+
+
+}
+
+const permutations = arr => {
+    if (arr.length <= 2) return arr.length === 2 ? [arr, [arr[1], arr[0]]] : arr;
+    return arr.reduce(
+        (acc, item, i) =>
+        acc.concat(
+            permutations([...arr.slice(0, i), ...arr.slice(i + 1)]).map(val => [
+                item,
+                ...val,
+            ])
+        ), []
+    );
+};
+
+
+c(solve([0, 2, 3, 0]))
