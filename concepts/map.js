6d13d2795cabfb33246ef9b50c7da0db46f2f901
Sakil <mdsakiluzzaman@gmail.com>
2022-07-19 13:01:07 +0600

    some changes

---
+++ concepts/map.js
@@ -0,0 +1,14 @@
+// var myMap = {};
+
+// myMap[newKey] = newValue;
+// ...
+
+
+// Another method
+var myMap = new Map();
+myMap.set("key0", "value");
+// ...
+myMap.has("key1");
+
+console.log(myMap.has('key0'))
+console.log(myMap.get('key0'))
--- test.js
+++ test.js
@@ -1,49 +1,14 @@
 const c = console.log.bind(console);
 
 
+var numberOfLines = function(widths, s) {
+    let alphabates = "abcdefghijklmnopqrstuvwxyz";
+    let map = new Map();
+    for (let i in alphabates) {
+        map.set(alphabates[i], widths[i])
 
-
-
-let p = new Promise((resolve, reject) => {
-    let a = 1 + 2;
-    if (a == 2) {
-        resolve("oh yeah done babe");
-
-    } else {
-        reject("shit error thrown")
     }
-})
 
-p.then((message) => {
-    c(message)
-}).catch(message => {
-    c(message);
-})
+};
 
-// c("starting")
-
-// setTimeout(() => {
-//     console.log("waited some seconds");
-// }, 1000);
-
-
-// c("end")
-
-
-function resolveAfter2Seconds() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve('resolved');
-        }, 2000);
-    });
-}
-
-async function asyncCall() {
-    console.log('calling');
-    const result = await resolveAfter2Seconds();
-    console.log(result);
-    console.log("calling Ended")
-        // expected output: "resolved"
-}
-
-asyncCall();
+c(numberOfLines([4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10], "abcdefghijklmnopqrstuvwxyz"))
