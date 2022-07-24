c5108f779a6f5bc2435fe5d698075225f9529809
Sakil <mdsakiluzzaman@gmail.com>
2022-07-24 13:53:23 +0600

    some changes

--- --arrayRotation.js
+++ --arrayRotation.js
@@ -36,13 +36,27 @@
 
 
 const c = console.log.bind(console);
-//also time limit exceedes
+// //also time limit exceedes
+// var rotate = function(nums, k) {
+//     while (k--) {
+//         nums.unshift();
+//     }
+//     return nums.pop();
+// };
+
 var rotate = function(nums, k) {
-    while (k--) {
-        nums.unshift();
-    }
-    return nums.pop();
+    const len = nums.length
+    k = (k % len)
+        // return k;
+
+    let end = nums.splice(len - k)
+        // return end;
+
+
+    nums.splice(0, 0, ...end);
+    return nums;
 };
+
 
 
 c(rotate([1, 2, 3, 4, 5, 6, 7], 3));
