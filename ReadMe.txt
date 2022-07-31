e6b72b08cf88b2006401e14056ca44674fb0a80d
Sakil <mdsakiluzzaman@gmail.com>
2022-07-31 11:51:30 +0600

    medium problem solved with new type

--- ReadMe.txt
+++ ReadMe.txt
@@ -4,4 +4,5 @@
 4."+++" means some test case failed and solved with community help.
 4."###" similar problems arises after submission.
 5.Exercism Problems needs package.json to run multifile problems.
-6.__E__ means easy problems ,__M__ / __H__ hard .
+6.__E__ means easy problems ,__M__ / __H__ hard.
+8."$$$" has optimized time complexity solution on other languages ..need to implement
--- __M__$$$_dailyTemperatures.js
+++ __M__$$$_dailyTemperatures.js
@@ -3,6 +3,8 @@
 
 const c = console.log.bind(console);
 
+//time complexity around 3000ms 
+
 var dailyTemperatures = function(temperatures) {
     const tempCopy = [...temperatures];
     return temperatures.map((item, i) => {
@@ -22,5 +24,23 @@ var dailyTemperatures = function(temperatures) {
         returnItem = undefined;
     });
 };
+
+//c++ code hase around 400ms
+// class Solution {
+//     public:
+//         vector<int> dailyTemperatures(vector<int>& T) {
+//             int n = T.size();
+//             stack<int> s;
+//             vector<int> ans (n);
+
+//             for(int i=n-1;i>=0;--i){
+//                 while(!s.empty() && T[i] >= T[s.top()]) s.pop();
+//                 ans[i] = s.empty() ? 0 : s.top()-i;
+//                 s.push(i);
+//             }
+
+//             return ans;
+//         }
+//     };
 
 c(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]));
