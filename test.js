7888d6ec9ee9cc9bc2f82fffc3bb4d3676f20b45
Sakil <mdsakiluzzaman@gmail.com>
2022-08-02 18:36:15 +0600

    some changes

--- test.js
+++ test.js
@@ -1,129 +1,81 @@
+// https://leetcode.com/problems/merge-intervals/
+//unfinished need to solve this shit 
 const c = console.log.bind(console);
 
-var fullJustify = function(words, maxWidth) {
-    let sentenceWords = [];
-    let resArray = [];
-    let sentence = " ";
-    let i = 0;
-    let finalArray = [];
-    while (i < words.length) {
-        if (sentence.length < maxWidth) {
-            sentenceWords.push(words[i]);
-            sentence = concatWords(sentenceWords);
-            i++;
-            c("sldfjsl;ajdf");
-
-        }
-        if (sentence.length > maxWidth) {
-            i--;
-            c('fuck')
-            sentenceWords.splice(-1);
-            // c(sentenceWords)
-            sentence = concatWords(sentenceWords);
-            resArray.push(sentenceWords);
-            sentenceWords = [];
-            sentence = " ";
-
-        }
-        // c(i);
+var merge = function(intervals) {
+    if (intervals.length == 1) {
+        return intervals;
     }
-    let resWordcounter = 0;
-    resArray.forEach((item) => {
-        item.forEach((item) => {
-            // c(item);
-            resWordcounter++;
-        });
-    });
+    let sortedIntervals = intervals.sort((a, b) => a[0] - b[0]);
+    let res = [];
 
-    resArray.push(words.splice(-(words.length - resWordcounter)));
-    for (i of resArray) {
-        let numOfSpace = i.length - 1;
-        // c("numOfSpace :" + numOfSpace);
-        let extraSpaceNeeded = maxWidth - concatWords(i).length;
-        // c("extraSpaceNeeded :" + extraSpaceNeeded);
-        // c(concatWords(i, extraSpaceNeeded, numOfSpace).length);
-        let _resSentence = addExtraSpaces(i, extraSpaceNeeded, numOfSpace);
-        finalArray.push(_resSentence);
-    }
-
-    return finalArray;
-
-
-    // return resWordcounter;
-};
-
-const addExtraSpaces = (arr, extraSpace = null, numOfSpace = null) => {
-    let sentence = "";
-    let spaceCount = " ";
-
-    // for (let i of arr) {
-    //     if (extraSpace && numOfSpace) {
-    //         let whileIteration = Math.floor(extraSpace / numOfSpace);
-    //         // c(whileIteration);
-    //         // c("whileIteration :" + whileIteration);
-    //         let i = 0;
-    //         while (i < whileIteration) {
-    //             spaceCount += " ";
-    //             i++;
+    // sortedIntervals.map((item, i) => {
+    //     if (i < sortedIntervals.length - 1) {
+    //         if (item[1] < sortedIntervals[i + 1][0]) {
+    //             res.push(item[0], sortedIntervals[i + 1][1]);
     //         }
     //     }
-    // }
-    if (extraSpace && numOfSpace) {
-        let whileIteration = Math.floor(extraSpace / numOfSpace);
-        // c(whileIteration);
-        // c("whileIteration :" + whileIteration);
-        let i = 0;
-        while (i < whileIteration) {
-            spaceCount += " ";
-            i++;
-        }
-    }
 
+    // })
+    let i = 0;
 
-    for (let i in arr) {
-        if (i == arr.length - 1) {
-            sentence += arr[i];
-
-
-
-            if (numOfSpace == 0) {
-                let j = 0;
-                while (j < extraSpace) {
-                    sentence += " ";
-                    j++;
+    while (i < sortedIntervals.length) {
+        let j = 0;
+        if (sortedIntervals[i + 1]) {
+            if (sortedIntervals[i][1] >= sortedIntervals[i + 1][0]) {
+                if (sortedIntervals[i + 1][1] > sortedIntervals[i][1]) {
+                    res.push([sortedIntervals[i][0], sortedIntervals[i + 1][1]])
+                } else {
+                    res.push([sortedIntervals[i][0], sortedIntervals[i][1]]);
                 }
 
+                i++;
+            } else {
+                res.push(sortedIntervals[i]);
+
             }
-            c("sentence Length :" + arr[i] + ' ' + sentence.length);
-            return sentence;
+
+
         }
-        sentence += arr[i] + spaceCount;
+        i++;
+
     }
-
-    return sentence;
-};
-const concatWords = (arr) => {
-    let sentence = "";
-    c("wassup");
-    for (let i in arr) {
-        if (i == arr.length - 1) {
-            sentence += arr[i];
-
-            return sentence;
-        }
-
-        sentence += arr[i] + " ";
+    let resCopy = [...res];
+    // return sortedIntervals.splice(-1)[0][1];
+    // return res.splice(-1)[0][1];
+    // return sortedIntervals[sortedIntervals.length - 1][1]
+    if (sortedIntervals[sortedIntervals.length - 1][1] != res[res.length - 1][1]) {
+        resCopy.push(intervals[intervals.length - 1]);
+        // c();
     }
-    return sentence;
-
+    return resCopy;
 };
 
-// c(fullJustify(["What", "must", "be", "acknowledgment", "shall", "be"], 16));
 c(
-    fullJustify(
-        ["This", "is", "an", "example", "of", "text", "justification."],
-        16
+    merge(
+        [
+            [1, 3],
+            [2, 6],
+            [8, 10],
+            [10, 18],
+            [19, 21],
+            [22, 24]
+        ],
     )
 );
 
-// c(fullJustify(["Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do"], 20));
+c(merge(
+    [
+        [1, 4],
+        [4, 5]
+    ]));
+
+
+c(merge([
+    [1, 3]
+]));
+
+c(merge([
+    [1, 4],
+    [2, 3]
+]));
--- test2.js
+++ test2.js
@@ -1,63 +1,130 @@
-// https://leetcode.com/problems/longest-consecutive-sequence/submissions/
+// https://leetcode.com/problems/text-justification/
 const c = console.log.bind(console);
-// test cases with minus value not working and last index not counting
-var longestConsecutive = function(nums) {
-    if (nums.length == 0) {
-        return 0;
-    }
-    let sortedNum = [...new Set(nums.sort((a, b) => a - b))];
-    c(sortedNum);
 
-    let resArrayCount = [];
-    let arrays = [];
-    let itemCount = 0;
-    let array = [];
-    sortedNum.forEach((item, i) => {
-        // c(sortedNum[i + 1]);
-        // c(Math.abs((Math.abs(sortedNum[i + 1]) - Math.abs(item))));
-        // if (sortedNum[i + 1]) {
-        // c(i);
-        // c(sortedNum[i + 1]);
-        // c((nums[i + 1] - (nums[i])));
+var fullJustify = function(words, maxWidth) {
+    let sentenceWords = [];
+    let resArray = [];
+    let sentence = " ";
+    let i = 0;
+    let finalArray = [];
+    while (i < words.length) {
+        if (sentence.length < maxWidth) {
+            sentenceWords.push(words[i]);
+            sentence = concatWords(sentenceWords);
+            i++;
+            c("sldfjsl;ajdf");
 
-        if (Math.abs(Math.abs(sortedNum[i + 1]) - Math.abs(sortedNum[i])) == 1) {
-            // c((nums[i + 1] - (nums[i])));
-            // c(i);
-            array.push(item);
-            // c(array);
-            itemCount++;
-        } else {
-            // c(array)
-            arrays.push(array);
-            resArrayCount.push(itemCount);
-
-            arrays = [];
-            itemCount = 0;
-            // c("else");
         }
-        // }
-    });
-    // c(itemCount);
-    if (resArrayCount.length == 0) { //if all elements are in sequence or last all elements are in sequence 
-        resArrayCount.push(itemCount);
+        if (sentence.length > maxWidth) {
+            i--;
+            c('fuck')
+            sentenceWords.splice(-1);
+            // c(sentenceWords)
+            sentence = concatWords(sentenceWords);
+            resArray.push(sentenceWords);
+            sentenceWords = [];
+            sentence = " ";
+
+        }
+        // c(i);
     }
-    // c(resArrayCount);
-    c(arrays);
-    // if (resArrayCount[0] == 0) {
-    //     return 0
+    let resWordcounter = 0;
+    resArray.forEach((item) => {
+        item.forEach((item) => {
+            // c(item);
+            resWordcounter++;
+        });
+    });
+
+    resArray.push(words.splice(-(words.length - resWordcounter)));
+    for (i of resArray) {
+        let numOfSpace = i.length - 1;
+        // c("numOfSpace :" + numOfSpace);
+        let extraSpaceNeeded = maxWidth - concatWords(i).length;
+        // c("extraSpaceNeeded :" + extraSpaceNeeded);
+        // c(concatWords(i, extraSpaceNeeded, numOfSpace).length);
+        let _resSentence = addExtraSpaces(i, extraSpaceNeeded, numOfSpace);
+        finalArray.push(_resSentence);
+    }
+
+    return finalArray;
+
+
+    // return resWordcounter;
+};
+
+const addExtraSpaces = (arr, extraSpace = null, numOfSpace = null) => {
+    let sentence = "";
+    let spaceCount = " ";
+
+    // for (let i of arr) {
+    //     if (extraSpace && numOfSpace) {
+    //         let whileIteration = Math.floor(extraSpace / numOfSpace);
+    //         // c(whileIteration);
+    //         // c("whileIteration :" + whileIteration);
+    //         let i = 0;
+    //         while (i < whileIteration) {
+    //             spaceCount += " ";
+    //             i++;
+    //         }
+    //     }
     // }
-    return resArrayCount.sort((a, b) => b - a)[0];
+    if (extraSpace && numOfSpace) {
+        let whileIteration = Math.floor(extraSpace / numOfSpace);
+        // c(whileIteration);
+        // c("whileIteration :" + whileIteration);
+        let i = 0;
+        while (i < whileIteration) {
+            spaceCount += " ";
+            i++;
+        }
+    }
+
+
+    for (let i in arr) {
+        if (i == arr.length - 1) {
+            sentence += arr[i];
+
+
+
+            if (numOfSpace == 0) {
+                let j = 0;
+                while (j < extraSpace) {
+                    sentence += " ";
+                    j++;
+                }
+
+            }
+            c("sentence Length :" + arr[i] + ' ' + sentence.length);
+            return sentence;
+        }
+        sentence += arr[i] + spaceCount;
+    }
+
+    return sentence;
+};
+const concatWords = (arr) => {
+    let sentence = "";
+    c("wassup");
+    for (let i in arr) {
+        if (i == arr.length - 1) {
+            sentence += arr[i];
+
+            return sentence;
+        }
+
+        sentence += arr[i] + " ";
+    }
+    return sentence;
 
 };
 
-// c(longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]));
+// c(fullJustify(["What", "must", "be", "acknowledgment", "shall", "be"], 16));
+c(
+    fullJustify(
+        ["This", "is", "an", "example", "of", "text", "justification."],
+        16
+    )
+);
 
-// // c(longestConsecutive([100, 4, 200, 1, 3, 2, 6, 7, 8, 9, 10]));
-// c(longestConsecutive([0]));
-// // c(longestConsecutive([0, 1, 2, 3, 4, 6, 7, 8, 10]))
-// c(longestConsecutive([3, 0, -1, -2, -3, ]))
-
-// // c(longestConsecutive([]));
-// c(longestConsecutive([1, 2, 0, 1]));
-
-c(longestConsecutive([-7, -1, 3, -9, -4, 7, -3, 2, 4, 9, 4, -9, 8, -7, 5, -1, -7]));
+// c(fullJustify(["Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do"], 20));
