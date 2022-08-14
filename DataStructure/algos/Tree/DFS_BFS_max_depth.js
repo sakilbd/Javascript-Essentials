aab01a74e00ea65db80241189101fbf8f3f74740
Sakil <mdsakiluzzaman@gmail.com>
2022-08-14 12:37:04 +0600

    some changes

---
+++ DataStructure/algos/Tree/DFS_BFS_max_depth.js
@@ -0,0 +1,63 @@
+const c = console.log.bind(console);
+
+var root = {
+    val: 5,
+    left: {
+        val: 1,
+        left: null,
+        right: null,
+    },
+    right: {
+        val: 8,
+        left: {
+            val: 6,
+            left: null,
+            right: null,
+        },
+        right: {
+            val: 9,
+            left: null,
+            right: null,
+        },
+    },
+};
+
+//Breath First Search Approach
+var maxDepth = function(root) {
+    let levelCount = 0;
+    let queue = [];
+    let data = [];
+    if (!root) return [];
+    queue.push(root);
+
+    while (queue.length > 0) {
+        let level = [];
+        let newQueue = [];
+
+        // let current = queue.shift();
+        // data.push(current.val);
+        // c(queue.length)
+        for (let node of queue) {
+            level.push(node.val);
+            if (node.left) {
+                newQueue.push(node.left);
+            }
+            if (node.right) {
+                newQueue.push(node.right);
+            }
+        }
+        levelCount++;
+        data.push(level);
+        queue = newQueue;
+    }
+    return levelCount;
+};
+
+//Depth First Search Approach 
+// var maxDepth = function(root) {
+//     if(!root) return null;
+//       let max = Math.max(maxDepth(root.left), maxDepth(root.right));
+//       return max + 1;
+//   };
+
+c(maxDepth(root));
