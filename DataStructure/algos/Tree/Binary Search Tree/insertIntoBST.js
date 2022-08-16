d777e1fdbde478d075470a6de9c434f284d60d65
Sakil <mdsakiluzzaman@gmail.com>
2022-08-16 11:12:54 +0600

    some changes

---
+++ DataStructure/algos/Tree/Binary Search Tree/insertIntoBST.js
@@ -0,0 +1,110 @@
+// https://leetcode.com/problems/insert-into-a-binary-search-tree/
+const c = console.log.bind(console);
+var root = {
+    val: 5,
+    left: {
+        val: 1,
+        left: null,
+        right: null,
+    },
+    right: {
+        val: 8,
+        left: {
+            val: 6,
+            left: null,
+            right: null,
+        },
+        right: {
+            val: 9,
+            left: null,
+            right: null,
+        },
+    },
+};
+// var root = {
+
+// };
+
+function TreeNode(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+    rootInsert(root) {
+        if (root == null) {
+            this.root = null;
+        } else {
+            this.root = root;
+        }
+    }
+    insert(val) {
+        var newNode = new TreeNode(val);
+
+        if (this.root === null) {
+            this.root = newNode;
+            return this.root;
+        } else {
+            var current = this.root;
+            while (true) {
+                if (val === current.val) return undefined;
+                if (val < current.val) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this.root;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (val > current.val) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this.root;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+        return this.root;
+        // return JSON.stringify(this.root);
+    }
+
+}
+
+var insertIntoBST = function(root, val) {
+
+    let tree = new BinaryTree();
+    if (root == null) { //this works in leetcode 
+        return tree.insert(val);
+    }
+    // if (Object.entries(root).length === 0) { //but this works for offline testing
+    //     return tree.insert(val);
+    // }
+    else {
+        tree.rootInsert(root);
+    }
+    return tree.insert(val);
+
+};
+
+c(insertIntoBST(root, 62));
+// c(searchBST(root, 8))
+
+//     10
+//  5       13
+//2   7  11    16
+
+
+// c(tree.insert(root));
+
+// var searchBST = function(root, val) {
+//     var tree = new BinaryTree();
+//     let treeRoot = tree.insert(root);
+//     // c(treeRoot)
+//     return tree.find(5)
+//         // return root
+// };
