a3c7b052798f2439cd7515da07948f0834e5e976
Sakil <mdsakiluzzaman@gmail.com>
2022-08-28 17:59:30 +0600

    some c hanges

--- Must Revisit/&&&__M__matrixSortDiagonally.js
+++ Must Revisit/&&&__M__matrixSortDiagonally.js
@@ -2,6 +2,8 @@
 
 const c = console.log.bind(console);
 
+
+//try by me couldnot solve shit!!!!!!!
 var diagonalSort = function(mat) {
     let arr = [];
     let tempArr = [];
@@ -46,6 +48,31 @@ var diagonalSort = function(mat) {
     // return arr;
     return arr;
 };
+
+
+//from community 
+// var diagonalSort = function(mat) {
+//     let map = {}
+//     for(let i=0;i<mat.length;i++){
+//         for(let j=0;j<mat[0].length;j++){
+//             let d = i-j
+//             if(!map[d]){
+//                 map[d]=[]
+//             }
+//             map[d].push(mat[i][j])
+//         }
+//     }
+//     for(let key in map){
+//         map[key].sort((a,b)=>{return a-b})
+//     }
+//     for(let i=0;i<mat.length;i++){
+//         for(let j=0;j<mat[0].length;j++){
+//             let d = i-j
+//             mat[i][j] = map[d].shift()
+//         }
+//     }
+//     return mat
+// };
 
 c(
     diagonalSort([
