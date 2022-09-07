d74f46db8d8f6faba901ecb87e663c850471c8ee
Sakil <mdsakiluzzaman@gmail.com>
2022-09-07 12:09:10 +0600

    some changes

---
+++ test.js
@@ -0,0 +1,36 @@
+const c = console.log.bind(console);
+
+var canJump = function(nums) {
+    if (nums.length == 1) {
+        return true;
+    }
+    let heightValue = 0;
+    let count = 0;
+    for (let i in nums) {
+        count -= 1;
+        if (nums[i] >= heightValue) {
+            heightValue = nums[i];
+            count = heightValue;
+        }
+        if (count == 0) {
+            // c(nums[i])
+            heightValue = nums[i];
+            count = heightValue;
+            if (heightValue == 0) {
+                if (i == nums.length - 1) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        // c("count :" + count)
+    }
+    return true;
+};
+
+// c(canJump([3, 2, 1, 0, 4]));
+// c(canJump([2, 3, 1, 1, 4]));
+// c(canJump([1, 2, 0, 1, 0, 4]));
+// c(canJump([2, 2, 0, 3, 0, 0, 4]));
+c(canJump([5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0]))
