406a34a5296caca06f420f2b7e1fea8c2812122c
unknown <mdsakiluzzaman@gmail.com>
2022-10-01 21:57:05 +0600

    some changes

--- test.js
+++ test.js
@@ -1,39 +1,13 @@
-// https://leetcode.com/problems/making-file-names-unique/
 const c = console.log.bind(console);
 
-var equalFrequency = function(word) {
-    let map = new Map();
 
-    word.split('').forEach(item => {
-        map.set(item, (map.get(item) + 1 || 1))
-    })
-    let newMap = new Map();
-    for (let [key, value] of map.entries()) {
-        // c(key)
-        newMap.set(value, key);
-
-    }
-    if (newMap.size > 2) {
-        return false;
-
-    } else if (newMap.size == 2) {
-        let arr = [];
-        for (let [key, value] of newMap.entries()) {
-            arr.push(key);
+var numberOfPairs = function(nums1, nums2, diff) {
+    for (let i in nums1) {
+        for (let j = i + 1; j < nums1.length; j++) {
+            c(nums1[i], nums1[j])
         }
-        return Math.abs(arr[0] - arr[1]) == 1 ? true : false;
-    } else if (newMap.size == 1) {
-        let [key] = newMap.keys();
-        return key == 1 ? true : false;
-        // return map.get(0)
     }
 };
 
 
-c(equalFrequency("aabbcccc"))
-
-c(equalFrequency('aazz'))
-
-c(equalFrequency('bac'))
-
-c(equalFrequency("ddaccb"))
+c(numberOfPairs([3, 2, 5], [2, 2, 1], 1))
