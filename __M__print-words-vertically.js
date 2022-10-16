6dc7eb18bad1b0c78a145d7f1637558c4ccf3bd5
Sakil <mdsakiluzzaman@gmail.com>
2022-10-16 17:44:15 +0600

    medium problem solved

---
+++ __M__print-words-vertically.js
@@ -0,0 +1,74 @@
+const c = console.log.bind(console);
+
+var printVertically = function(s) {
+    let splited = s.split(" ");
+    let finalArr = [];
+    let splitedCopy = [...splited];
+    let length = splitedCopy.sort((a, b) => {
+        return -a.length + b.length;
+    })[0].length;
+    // return length;
+    let j = 0;
+    let string = "";
+    for (j; j < length; j++) {
+        splited.forEach((item) => {
+            for (let i = 0; i < 1; i++) {
+                // item[j] ? c(item[j]) : c("E");
+                item[j] ? (string += item[j]) : (string += " ");
+            }
+
+            if (j == length) {
+                j = 0;
+            }
+
+            // c(string)
+        });
+        finalArr.push(string);
+        string = "";
+    }
+    let stringJoinFlag = 0;
+    return finalArr.map((item) => {
+        let split = item.split("");
+
+        split.reverse();
+        // return split;
+        let returnString = "";
+        // let splitCopy = [...split];
+
+        // c(split)
+        split.forEach((item, i) => {
+
+            // c(item[i])
+
+            if (item != " ") {
+                // c("fuck")
+                // string +=
+                // c("I :" + i);
+                // c(splitCopy.splice(i - 1, 1));
+                // c(splitCopy);
+                // c("shit");
+                stringJoinFlag = 1;
+            }
+            if (stringJoinFlag == 1) {
+                if (item == ' ') {
+                    returnString += ' ';
+                } else {
+                    returnString += item
+                        // c(item)
+                }
+                // returnString += item[i];
+            }
+
+        });
+        stringJoinFlag = 0;
+
+        return returnString.split('').reverse().join('');
+        // return item;
+    });
+
+
+};
+
+// c(printVertically("HOW ARE YOU"))
+
+c(printVertically("TO BE OR NOT TO BE"));
