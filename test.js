bcd93e8a4c1fd989de567d4fc2d5fc1aaa687b2f
Sakil <mdsakiluzzaman@gmail.com>
2022-10-17 13:21:52 +0600

    some updates

--- test.js
+++ test.js
@@ -1,20 +1,61 @@
 const c = console.log.bind(console);
 
-var printVertically = function(s) {
-    const arr = s.split(' ')
-    const maxLength = Math.max(...arr.map(word => word.length))
-
-    return [...Array(maxLength)].map((_, i) => arr.map(word => word[i] || ' ').join('').trimEnd())
+var maxProduct = function(words) {
+    let resProduct = 0;
+    let count = 0;
+    words.sort((a, b) => a.length - b.length);
+    // return words;
+    words.forEach((item) => {
+        for (let i = words.length - 1; i >= 0; i--) {
+            let wordSplit = words[i].split("");
+            let itemSplit = item.split("");
+            let wordSet = [...new Set(wordSplit)];
+            let itemSet = [...new Set(itemSplit)];
+            if (itemSet.length < wordSet.length) {
+                var check = itemSet.filter((x) => {
+                    // c(x)
+                    return !wordSet.includes(x);
+                });
+            } else {
+                // c("shit")
+                var check = wordSet.filter((x) => {
+                    // c(x)
+                    return !itemSet.includes(x);
+                });
+            }
+            if (check.length == wordSet.length) {
+                count++;
+                // c(count)
+                // c("item :" + item);
+                // c("words :" + words[i]);
+                let product = item.length * words[i].length;
+                if (product > resProduct) {
+                    resProduct = product;
+                    break;
+                }
+            }
+            // c("ITEM :" + item.length)
+        }
+    });
+    return resProduct;
 };
 
+c(maxProduct(["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]));
+c(maxProduct(["a", "ab", "abc", "d", "cd", "bcd", "abcd"]));
 
-var test = (s) => {
-    return s.trimEnd().split('');
+c(maxProduct(["a", "aa", "aaa", "aaaa"]));
 
-}
-
-// c(printVertically("HOW ARE YOU"))
-
-c(test("  sakil  "))
-
-// c(printVertically("TO BE OR NOT TO BE"));
+c(
+    maxProduct([
+        "eae",
+        "ea",
+        "aaf",
+        "bda",
+        "fcf",
+        "dc",
+        "ac",
+        "ce",
+        "cefde",
+        "dabae",
+    ])
+);
