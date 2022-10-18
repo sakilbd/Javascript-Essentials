677ad74478c64c60c4cfa43e54c19b828307e105
Sakil <mdsakiluzzaman@gmail.com>
2022-10-18 18:11:21 +0600

    sdf

--- test.js
+++ test.js
@@ -1,29 +1,34 @@
-const c = console.log.bind(console)
-var maxProduct = function(words) {
-    let arr = [];
-    words.forEach(str => {
-        let cur = 0;
-        for (let i = 0; i < str.length; i++) {
+// https://leetcode.com/problems/number-of-pairs-of-strings-with-concatenation-equal-to-target/
+const c = console.log.bind(console);
 
-            cur |= 1 << (str[i].charCodeAt() - 97)
-
-        }
-        arr.push(cur)
-
-    })
-    let max = 0
-    for (let i in arr) {
-
-        for (let j in arr) {
-
-            if ((arr[i] & arr[j]) === 0) {
-
-                max = Math.max(max, words[i].length * words[j].length)
+var numOfPairs = function(nums, target) {
+    let count = 0;
+    let map = new Set();
+    nums.forEach((item1, j) => {
+        nums.forEach((item, i) => {
+            // if (j == 2) {
+            //     c(item)
+            // }
+            if (i != j) {
+                // c("I :" + i);
+                // c("J :" + j)
             }
-        }
-    }
-    return max;
+
+            if (i != j) {
+                if (item1 + item == target) {
+                    map.add(i.toString() + (j).toString());
+                    count++;
+                    // c(item1 + item);
+                }
+            }
+
+        });
+    });
+    return map;
+    // return count;
 };
 
-// c(maxProduct(["xyz"]));
-c(maxProduct(["abcd", "baz", "foo", "bar", "xtfn", "abcdef"]));
+c(numOfPairs(["777", "7", "77", "77"], "7777"));
+
+c(numOfPairs(["123", "4", "12", "34"], "1234"))
+c(numOfPairs(["1", "1", "1"], "11"))
