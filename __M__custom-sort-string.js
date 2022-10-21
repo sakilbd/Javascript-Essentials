25e3a4922b7406ebb82f9750a1c290f0fc30a8d5
unknown <mdsakiluzzaman@gmail.com>
2022-10-21 19:50:13 +0600

    some changes

--- __M__custom-sort-string.js
+++ __M__custom-sort-string.js
@@ -1,6 +1,9 @@
+// https://leetcode.com/problems/custom-sort-string/
+
 const c = console.log.bind(console);
 
 
+//by me 
 var customSortString = function(order, s) {
     let str = '';
     let map = new Map();
@@ -14,7 +17,24 @@ var customSortString = function(order, s) {
     })
     return str + s.split('').filter(item => !str.split('').includes(item)).join('')
 };
-
+var customSortString = function(order, s) {
+    /*res = ""
+    
+    for(let i = 0; i < order.length; i++) {
+        index = s.indexOf(order[i])
+        while(index !== -1) {
+            res += s[index]
+            s = s.slice(0, index) + s.slice(index + 1)
+            index = s.indexOf(order[i])
+        }
+    }
+    if(s.length !== 0) {
+        res += s
+    }
+    return res
+    */
+    return s.split('').sort((a, b) => order.indexOf(a) - order.indexOf(b)).join('')
+};
 
 c(customSortString("cba", "abcd"))
 c(customSortString("cbafg", "abcd"))
