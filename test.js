cec66d1f42e464743cd03106f3f1c71ddc0a1391
sakilbd <mdsakiluzzaman.com>
2022-10-30 16:02:19 +0600

    some changes

--- test.js
+++ test.js
@@ -2,27 +2,48 @@ const c = console.log.bind(console);
 
 var partitionLabels = function(s) {
     let res = [];
+    let sliceStart = 0;
+    s = s + " ";
 
+    for (let i = 1; i < s.length; i++) {
 
-    let temp = '';
-    let spliceStartIndex = 0
-    for (let i = 0; i < s.length; i++) {
-        let split = s.split('');
-        let spliced = split.splice(i, i);
+        let left = s.slice(sliceStart, i)
+        let right = s.slice(i, s.length);
 
-        let commonItems = spliced.filter(item => split.includes(item));
+        let commonItems = [...new Set(left.split(''))].filter(item => [...new Set(right.split(''))].includes(item));
         if (commonItems == "") {
-            // spliceStartIndex = i;
-            res.push(spliced.join(''))
-        }
-        // c("spliced :" + spliced);
-        // c("commonItems :" + commonItems)
-        // c("split :" + split);
+            res.push(left);
+            sliceStart = i;
 
+        }
 
     }
-    return res;
+    return res.map(item => item.length);
 };
 
 
+//from community with less runtime 
+// var partitionLabels = function(s) {
+//     var lastOccurence = {};
+//     for (let i = 0; i < s.length; i++) {
+//         lastOccurence[s[i]] = i;
+//     }
+//     var start = -1,
+//         end = -1,
+//         ans = [];
+//     for (let i = 0; i < s.length; i++) {
+//         end = Math.max(end, lastOccurence[s[i]])
+//         if (i == end) {
+//             ans.push(end - start)
+//             start = i;
+//         }
+//     }
+
+//     return ans;
+// };
+
+
+
 c(partitionLabels("ababcbacadefegdehijhklij"));
+c(partitionLabels("eccbbbbdec"))
+    // c(partitionLabels("abcdefghijak"));
