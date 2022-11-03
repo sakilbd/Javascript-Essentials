31bd3c04f0bdc1782753aaa12814d48eb8f6a9fe
sakilbd <mdsakiluzzaman@gmail.com>
2022-11-03 18:15:29 +0600

    some changes

--- test.js
+++ test.js
@@ -55,20 +55,54 @@ const c = console.log.bind(console);
 //     return res;
 // };
 
-
 var strWithout3a3b = function(a, b) {
-    let res = ''
+    let res = "";
+    return a > b ? createString("a", "b", a, b) : createString("b", "a", b, a);
+};
+let createString = (first, second, firstCount, secondCount) => {
+    let numberOfPairsFirst =
+        parseInt(firstCount / 2) +
+        ((firstCount / 2) % 1 == 0 ? parseInt(0) : parseInt(1));
+    // return numberOfPairsFirst;
+    let secondPairNeeded = numberOfPairsFirst - 1;
+
+    let secondParilElemCount = parseInt(secondCount / secondPairNeeded);
+    // return secondPairNeeded;
+    let res = "";
     while (true) {
-        if (a < 2 && a > 0) {
-            res += "a";
-            a--;
-        }
-        if (b < 2 && )
-            if (a == 0 && b == 0) {
-                break;
+        if (numberOfPairsFirst > 1) {
+            if (firstCount >= 2) {
+                res += Array(2).fill(first).join('');
+                // c(Array(2).fill(first).join(''));
+                firstCount = firstCount - 2;
+            } else {
+                res += first;
+                firstCount--;
             }
+        } else {
+            res += first;
+            firstCount--;
+        }
+        // if (secondCount == 2) {
+        //     res += Array(2).fill(second);
+        //     secondCount = secondCount - 2;
+        // }
+
+        res += second;
+        secondCount--;
+        if (secondCount == 0) {
+            break;
+        }
+        if (firstCount == 0) {
+            break;
+        }
     }
+    return res;
 
-}
+    return secondParilElemCount;
+    return numberOfPairsFirst;
+    return numberOfPairsFirst;
+    // return (firstCount / 2) % 1 == 0
+};
 
-c(strWithout3a3b(2, 5));
+c(strWithout3a3b(6, 3));
