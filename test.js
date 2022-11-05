1b3f6626b8f42abab97c59c6a36478031fa41e5a
unknown <mdsakiluzzaman@gmail.com>
2022-11-05 21:13:16 +0600

    some changes

--- test.js
+++ test.js
@@ -1,136 +1,68 @@
 // https://leetcode.com/problems/string-without-aaa-or-bbb/
 const c = console.log.bind(console);
 
-// var strWithout3a3b = function(a, b) {
-//     let res = "";
-//     while (true) {
-//         if (a > b) {
-//             if (a >= 2) {
-//                 res += Array(2).fill("a").join("");
-//                 a = a - 2;
-//             } else if (a > 0) {
-//                 res += "a";
-//                 a--;
-//             }
-//             if (b >= 2) {
-//                 if (a > 2) {
-//                     res += "b";
-//                     b--;
-//                 } else {
-//                     res += Array(2).fill("b").join("");
-//                     b = b - 2;
-//                 }
-//             } else if (b > 0) {
-//                 res += "b";
-//                 b--;
-//             }
-//             if (a == 0 && b == 0) {
-//                 break;
-//             }
-//         } else {
-//             if (b >= 2) {
-//                 res += Array(2).fill("b").join("");
-//                 b = b - 2;
-//             } else if (b > 0) {
-//                 res += "b";
-//                 b--;
-//             }
-//             if (a >= 2) {
-//                 if (b > 2) {
-//                     res += "a";
-//                     a--;
-//                 } else {
-//                     res += Array(2).fill("a").join("");
-//                     a = a - 2;
-//                 }
-//             } else if (a > 0) {
-//                 res += "a";
-//                 a--;
-//             }
-//             if (a == 0 && b == 0) {
-//                 break;
-//             }
-//         }
-//     }
-//     return res;
-// };
 
-var strWithout3a3b = function(a, b) {
-    let res = "";
-    if (a == b) {
-        while (true) {
-            res += "a";
-            res += "b"
-            a--;
-            b--;
-            if (a == 0) {
-                break;
-            }
-        }
-        return res;
 
-    }
-    return a > b ? createString("a", "b", a, b) : createString("b", "a", b, a);
+const strWithout3a3b = function(a, b) {
+    return a > b ? calc("a", "b", a, b) : calc("b", "a", b, a);
 };
-let createString = (first, second, firstCount, secondCount) => {
-    let numberOfPairsFirst =
-        parseInt(firstCount / 2) +
-        ((firstCount / 2) % 1 == 0 ? parseInt(0) : parseInt(1));
-    // return numberOfPairsFirst;
-    let secondPairNeeded = numberOfPairsFirst - 1;
-    // return secondPairNeeded;
-    let secondParilElemCount = parseInt(
-        secondCount / (secondPairNeeded == 0 ? 1 : secondPairNeeded)
-    );
-    // let secondParilElemCount = parseInt(secondCount / secondPairNeeded);
 
-    // return secondParilElemCount;
-    let res = "";
+const calc = (first, second, fCount, sCount) => {
+    let secondCountTemp = sCount;
+    let firstArr = Array(fCount).fill(first);
+
+    let secondArrPredict = [];
+    let count = fCount;
     while (true) {
-        // return ;
-        if (numberOfPairsFirst > 1) {
-            if (firstCount >= 2) {
-                res += Array(2).fill(first).join("");
-                // c(Array(2).fill(first).join(''));
-                firstCount = firstCount - 2;
-            } else {
-                res += first;
-                firstCount--;
-            }
-        } else {
-
-            res += first;
-            firstCount--;
-
-        }
-        if (secondCount > 0) {
-            if (secondCount == 2 && firstCount < 3) {
-                res += Array(2).fill(second).join("");
-                secondCount = secondCount - 2;
-            } else {
-                let arrLengthCount =
-                    parseInt(secondParilElemCount) == 0 ?
-                    parseInt(1) :
-                    parseInt(secondParilElemCount);
-                // c(arrLengthCount);
-                res += Array(arrLengthCount).fill(second).join("");
-                secondCount--;
-            }
-        }
-        // c("fCount :" + firstCount);
-        // c("sCount :" + secondCount);
-        if (firstCount == 0 && secondCount == 0) {
+        if (count <= 0) {
             break;
         }
+        secondArrPredict.push(second);
+        count -= 2;
+        sCount--;
     }
-    return res;
 
-    return secondParilElemCount;
-    return numberOfPairsFirst;
-    return numberOfPairsFirst;
-    // return (firstCount / 2) % 1 == 0
+    if (sCount > 0) {
+        for (let i = 0; i < secondArrPredict.length; i++) {
+            if (sCount == 0) {
+                break;
+            }
+            secondArrPredict[i] = secondArrPredict[i] + second;
+            sCount--;
+        }
+    }
+    // return secondArrPredict;
+    // secondArrPredict.length > sCount ? secondArrPredict.pop() : '';
+    let res = "";
+    if (fCount >= 2) {
+        for (let i of secondArrPredict) {
+            res += firstArr.splice(-2).join("") + i;
+        }
+    } else {
+        res = first + second;
+    }
+
+    // secondArrPredict.splice(-2)
+    // if (secondArrPredict.length > sCount) {
+    //     let resSplit = res.split('');
+    //     resSplit.pop();
+    //     res = resSplit.join('');
+    // }
+    let secondCount = 0;
+    for (let i = 0; i < res.length; i++) {
+        res[i] == second ? secondCount++ : "";
+    }
+
+    // return secondCountTemp;
+    if (secondCount > secondCountTemp) {
+        let resSplit = res.split('');
+        resSplit.pop();
+        res = resSplit.join('');
+    }
+
+    return res;
 };
 
-// c(strWithout3a3b(4, 4));
-// c(strWithout3a3b(2, 5));
-c(strWithout3a3b(3, 4))
+c(strWithout3a3b(1, 1))
+    // c(strWithout3a3b(4, 3));
+    // c(strWithout3a3b(7, 6));
