88bb128dd0b75c504d4144b3de3daa8bf5c479a3
unknown <mdsakiluzzaman@gmail.com>
2022-11-05 21:16:20 +0600

    some changes

---
+++ __M__string-without-aaa-or-bbb.js
@@ -0,0 +1,82 @@
+// https://leetcode.com/problems/string-without-aaa-or-bbb/
+const c = console.log.bind(console);
+
+
+
+// const strWithout3a3b = function(a, b) {
+//     return a > b ? calc("a", "b", a, b) : calc("b", "a", b, a);
+// };
+
+// const calc = (first, second, fCount, sCount) => {
+//     let secondCountTemp = sCount;
+//     let firstArr = Array(fCount).fill(first);
+
+//     let secondArrPredict = [];
+//     let count = fCount;
+//     while (true) {
+//         if (count <= 0) {
+//             break;
+//         }
+//         secondArrPredict.push(second);
+//         count -= 2;
+//         sCount--;
+//     }
+
+//     if (sCount > 0) {
+//         for (let i = 0; i < secondArrPredict.length; i++) {
+//             if (sCount == 0) {
+//                 break;
+//             }
+//             secondArrPredict[i] = secondArrPredict[i] + second;
+//             sCount--;
+//         }
+//     }
+//     // return secondArrPredict;
+//     // secondArrPredict.length > sCount ? secondArrPredict.pop() : '';
+//     let res = "";
+//     if (fCount >= 2) {
+//         for (let i of secondArrPredict) {
+//             res += firstArr.splice(-2).join("") + i;
+//         }
+//     } else {
+//         res = first + second;
+//     }
+
+//     // secondArrPredict.splice(-2)
+//     // if (secondArrPredict.length > sCount) {
+//     //     let resSplit = res.split('');
+//     //     resSplit.pop();
+//     //     res = resSplit.join('');
+//     // }
+//     let secondCount = 0;
+//     for (let i = 0; i < res.length; i++) {
+//         res[i] == second ? secondCount++ : "";
+//     }
+
+//     // return secondCountTemp;
+//     if (secondCount > secondCountTemp) {
+//         let resSplit = res.split('');
+//         resSplit.pop();
+//         res = resSplit.join('');
+//     }
+
+//     return res;
+// };
+
+
+// from community 
+var strWithout3a3b = function(a, b) {
+    if (a === 0)
+        return 'b'.repeat(b)
+    if (b === 0)
+        return 'a'.repeat(a)
+    if (a === b)
+        return 'ab'.repeat(a)
+    if (a > b)
+        return 'aab' + strWithout3a3b(a - 2, b - 1)
+    return 'bba' + strWithout3a3b(a - 1, b - 2)
+};
+
+c(strWithout3a3b(1, 1))
+    // c(strWithout3a3b(4, 3));
+    // c(strWithout3a3b(7, 6));
