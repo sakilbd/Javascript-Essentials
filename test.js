00e61c25f90a77715899348fa528538f71a944e3
sakilbd <mdsakiluzzaman@gmail.com>
2022-11-10 18:25:40 +0600

    some changes

--- test.js
+++ test.js
@@ -1,31 +1,36 @@
+// https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
 const c = console.log.bind(console);
 
 var smallestSubsequence = function(s) {
+    let arr = [];
     let res = "";
     for (let i = 0; i < s.length; i++) {
         let slice = s.slice(i, s.length);
-
-        // slice.split('').some(item => item == s[i]) ? '' : res += s[i];
-        // c(
-        //     slice.split("").every((e, j, a) => {
-        //         // c(s[i])
-        //         c(a)
-        //         c(e)
-        //         c(j)
-        //             // c(a.indexOf(e) === j);
-        //         return a.indexOf(e) === j;
-
-        //     })
-        // );
-        // return
-        slice.split("").every((e, j, a) => a.indexOf(e) === j) ? res += s[i] : '';
-        // c(slice);
-
+        slice.split("").indexOf(s[i]) == slice.split("").lastIndexOf(s[i]) ? res += s[i] : '';
     }
+    let index = 0;
+    let slice = s.slice(s.indexOf(res[0]), s.length);
+    for (let i = 0; i < res.length; i++) {
+
+        if (index <= 0) {
+            index = s.indexOf(res[i]);
+        }
+
+        arr.push([res[i], slice.indexOf(res[i])])
+    }
+    arr.sort((a, b) => a[1] - b[1]);
+
+    let result = ''
+    for (let i in arr) {
+        result += arr[i][0];
+    }
+    return result;
+    return arr;
     return res;
 };
 
 // c(smallestSubsequence("bcabc"));
 
 // c(smallestSubsequence("cbacdcbc"))
-c(smallestSubsequence("abcc"))
+c(smallestSubsequence("cdadabcc"))
+    // c(smallestSubsequence("abdcc"));
