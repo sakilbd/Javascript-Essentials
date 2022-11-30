11a2b67aeadcd03c17052dc69b40660715f3e398
sakilbd <mdsakiluzzaman@gmail.com>
2022-11-30 13:07:16 +0600

    some changes

--- test.js
+++ test.js
@@ -1,27 +1,35 @@
+// https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/
+
 const c = console.log.bind(console);
 
 var numberOfSubstrings = function(s) {
-
     let length = s.length;
+
+    // return length
     let x = 0;
+    let count = 0;
     while (x <= length) {
         let y = x;
-
+        // c("X :" + x);
+        // c("Y :" + y);
         while (y <= length) {
-            c(s.slice(y, 3));
+            if (y < length - 2) {
 
-            c("Y :" + y);
+                let slice = s.slice(x, y + 3);
+                c([...new Set(slice.split(''))])
+
+            } else {
+                break
+            }
             y++;
-        }
-        c("X :" + x);
-        x++
+            // c("Y :" + y);
 
+        }
+
+        x++;
     }
 
 
-
-    return s.slice(1, 3)
 };
 
-
-c(numberOfSubstrings("abcabc"))
+c(numberOfSubstrings("abcabcaa"));
