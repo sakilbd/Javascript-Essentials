c3dd910f1745295fada94a876b3f40ebb1c2067e
sakilbd <mdsakiluzzaman@gmail.com>
2022-12-01 14:34:27 +0600

    string-compression

--- test.js
+++ test.js
@@ -1,49 +1,61 @@
+// https://leetcode.com/problems/string-compression/
 const c = console.log.bind(console);
 
 var compress = function(chars) {
-
     let curChar = chars[0];
-
-
     let count = 0;
     let temp = [...chars];
-
     temp.forEach((el, i) => {
-
         if (el === curChar) {
-            chars.reverse().pop();
-            chars.reverse();
+            chars.reverse().pop(); //needed as leetcode not accepting overriding chars array 
+            chars.reverse(); //needed as leetcode not accepting overriding chars array 
             count++;
+
+            // used this portion to push and calculate last based on last index 
             if (i == temp.length - 1) {
                 chars.push(curChar);
-                count > 1 ? chars.push(count.toString()) : "";
-
+                if (count >= 1 && count.toString().length == 1) {
+                    count > 1 ? chars.push(count.toString()) : "";
+                } else {
+                    count.toString().split('').forEach(item => {
+                        chars.push(item);
+                    })
+                }
             }
-            // c("if CurChar : " + curChar);
-            // c("if i : " + i)
-            // c("if count :" + count)
+            // used this portion to push and calculate last based on last index 
 
         } else {
             chars.reverse().pop();
             chars.reverse();
             chars.push(curChar);
-            count > 1 ? chars.push(count.toString()) : "";
-            // c(curChar);
-            // c(i)
-            // c("count :" + count)
+            if (count >= 1 && count.toString().length == 1) {
+                count > 1 ? chars.push(count.toString()) : "";
+            } else {
+                count.toString().split('').forEach(item => {
+                    chars.push(item); // for pushing multi digit value as individual element as required 
+                })
+            }
             curChar = el.toString();
             count = 1;
-            // c("new Char : " + curChar);
-            // c("new I : " + i)
+            //for pushing the last unique(single occurance) element 
+            if (i == temp.length - 1) {
+
+                chars.push(curChar);
+
+                count > 1 ? chars.push(count.toString()) : "";
+
+            }
+            //for pushing the last unique(single occurance) element 
 
         }
-        // c("global el :" + el)
-        // c("global i : " + i)
-        // c("global curChar :" + curChar)
 
     });
 
-    return chars
+    return chars; // return should be commented in submission 
 };
 
-c(compress(["p", "a", "a", "b", "b", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c"]));
+c(compress(["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"]))
+c(compress(
+    ["a"]));
+
+c(compress(["a", "b", "c"]))
