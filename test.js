48754bcbb971d10258ee136123c2af4d2fbef1bd
sakilbd <mdsakiluzzaman@gmail.com>
2022-12-01 12:10:18 +0600

    some changes

---
+++ test.js
@@ -0,0 +1,15 @@
+const c = console.log.bind(console);
+
+var camelMatch = function(queries, pattern) {
+    let patternArray = [];
+    let tampStr = '';
+    for (let i = 0; i < pattern.length; i++) {
+        if (pattern[i].toUpperCase() === pattern[i]) {
+
+        }
+    }
+
+};
+
+
+c(camelMatch(["FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"], "FooBar"))
