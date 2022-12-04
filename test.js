9b65ae090d427b03b9ec39342d7f845b76952323
sakilbd <mdsakiluzzaman@gmail.com>
2022-12-04 17:44:56 +0600

    some changes

---
+++ test.js
@@ -0,0 +1,97 @@
+const c = console.log.bind(console);
+
+var closeStrings = function(word1, word2) {
+    if (word1.length != word2.length) {
+        return false;
+    }
+    let word1_map = new Map();
+    let word2_map = new Map();
+
+    // word1_map.set("a", 2)
+    // word1_map.set("a", 2)
+
+    // for (let [key, value] of word1_map.entries()) {
+    //     console.log(key, value);
+    // }
+    // return word1_map.get("a")
+    // return word1_map.size;
+    for (let i = 0; i < word1.length; i++) {
+        word1_map.set(
+            word1_map.has(word1[i]) ? word1[i] : word1[i],
+            word1_map.has(word1[i]) ? word1_map.get(word1[i]) + 1 : 1
+        );
+        word2_map.set(
+            word2_map.has(word2[i]) ? word2[i] : word2[i],
+            word2_map.has(word2[i]) ? word2_map.get(word2[i]) + 1 : 1
+        );
+    }
+
+    // return word1_map
+    let values_word2 = [...word2_map.values()];
+    let values_word1 = [...word1_map.values()];
+
+    // return values;
+    if (word2_map.size != word1_map.size) {
+        return false;
+    }
+
+    for (let [key, value] of word1_map.entries()) {
+        // c(value)
+        // c(!values.includes(value))
+        // c(word2_map.get(key))
+        if (word2_map.get(key) > 0) {} else {
+            return false;
+        }
+        if (values_word2.includes(value) === false) {
+            return false;
+        }
+    }
+    for (let [key, value] of word2_map.entries()) {
+        // c(value)
+        // c(!values.includes(value))
+        // c(!word1_map.get(key))
+        if (word2_map.get(key) > 0) {} else {
+            return false;
+        }
+        if (values_word1.includes(value) === false) {
+            return false;
+        }
+    }
+
+    let word1_value_map = new Map();
+    let word2_value_map = new Map();
+
+    for (let i = 0; i < values_word1.length; i++) {
+        word1_value_map.set(
+            word1_value_map.has(values_word1[i]) ? values_word1[i] : values_word1[i],
+            word1_value_map.has(values_word1[i]) ?
+            word1_value_map.get(values_word1[i]) + 1 :
+            1
+        );
+        word2_value_map.set(
+            word2_value_map.has(values_word2[i]) ? values_word2[i] : values_word2[i],
+            word2_value_map.has(values_word2[i]) ?
+            word2_value_map.get(values_word2[i]) + 1 :
+            1
+        );
+    }
+    for (let [key, value] of word1_value_map.entries()) {
+        if (word2_value_map.get(key) != value) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+c(closeStrings("cabbba", "abbeee"));
+c(closeStrings("cabbba", "aabbss"));
+
+c(closeStrings("abbzzca", "babzzcq"));
+
+c(closeStrings("abbzzca", "babzzcz"));
+
+c(closeStrings("abc", "bca")); // true
+c(closeStrings("uau", "ssx")); //false
+
+c(closeStrings("aaabbbbccddeeeeefffff", "aaaaabbcccdddeeeeffff"));
