5f9c30e998b121aeb42d3de9c25a82d729c1cbb5
sakilbd <mdsakiluzzaman@gmail.com>
2022-12-12 13:20:40 +0600

    some chnages

--- try.js
+++ try.js
@@ -1,63 +1,52 @@
-const c = console.log.bind(console)
+const c = console.log.bind(console);
 
-var maxLength = function(arr) {
+let p = "&quot;...&quot;";
 
-    let i = 0;
-    let j = 0;
-    let length = 0;
-    let str = ''
-    arr.sort((a, b) => a.length - b.length)
-    while (true) {
-        for (i; i < arr.length; i++) {
-            for (j = i; j < arr.length; j++) {
-                if (arr[j].length == [...new Set(arr[j].split(''))].length) {
-                    let temp = str;
-                    // c([...new Set((temp + arr[j]).split(''))])
-                    if ((temp + arr[j]).length == [...new Set((temp + arr[j]).split(''))].length) {
-                        str += arr[j]
-                        if (str.length > length)
-                            length = str.length
+c(p.replaceAll("&quot;", "Q"));
+let map = {
+    "&quot;": '\\"', // "\" used to assign special character \"
+    "&apos;": "'",
+    "&amp;": "&",
+    "&gt;": ">",
+    "&lt;": "<",
+    "&frasl;": "/",
+};
+
+for (var key in map) {
+    c(key);
+}
+
+var entityParser = function(text) {
+    let map = {
+        "&quot;": '\\"', // "\" used to assign special character \"
+        "&apos;": "'",
+        "&amp;": "&",
+        "&gt;": ">",
+        "&lt;": "<",
+        "&frasl;": "/",
+    };
+
+    return text
+        .split(" ")
+        .map((item) => {
+            if (map[item]) {
+                return map[item];
+            } else {
+                // return item
+                let temp = item;
+                for (var key in map) {
+                    temp = temp.replaceAll(key, map[key])
+                    if (temp != item) {
+                        return temp;
                     }
 
                 }
-
+                return item
             }
-
-            j = 0
-            str = ''
-
-        }
-
-        if (i == arr.length) {
-            break;
-        };
-    }
-    return length;
+        })
+        .join(" ");
 };
-var maxLength = function(arr) {
-    let res = 0;
 
-    let solve = (start, curr) => {
-        if (curr.length !== new Set(curr.split('')).size) {
-            return "shit";
-        }
-
-        res = Math.max(res, curr.length);
-
-        for (let i = start; i < arr.length; i++) {
-            c(curr)
-            c(`${curr}${arr[i]}`)
-            c(solve(i + 1, `${curr}${arr[i]}`));
-        }
-    }
-
-    solve(0, '');
-
-    return res;
-};
-// c(maxLength(["un", "iq", "ue"]))
-c(maxLength(["cha", "r", "act", "ers"]));
-
-// c(maxLength(["a", "abc", "d", "de", "def"]))
-
-// c(maxLength(["ogud", "ejipchfydrgl", "b", "kjxmzhnuoisgqvawel", "mizlcgdqebwuotfnk", "bjvkrgeozidytquchlw", "tzjqwumkirxeal", "x", "rkpuabmo", "mxndpcqzua"])) //20
+c(entityParser("&amp; is an HTML entity but &ambassador; is not."))
+c(entityParser("and I quote: &quot;...&quot;"))
+c(entityParser("&amp;gt;"))
