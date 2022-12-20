fc32b79d67a0f78385a7919a5ca1d7bfc1f96acb
sakilbd <mdsakiluzzaman@gmail.com>
2022-12-20 14:25:06 +0600

    some changes

--- try.js
+++ try.js
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/queue-reconstruction-by-height/
 const c = console.log.bind(console);
 
+
+//updated infinite loop 
 var reconstructQueue = function(people) {
     people.sort((a, b) => {
         if (a[1] === b[1]) {
@@ -53,7 +55,52 @@ var reconstructQueue = function(people) {
             // c(checkArr);
         }
         count = 0;
-        // c(i)
+        c(i)
+            // }
+        i++;
+    }
+
+    return temp
+
+};
+
+
+//works forsome test cases except last test case
+var reconstructQueue = function(people) {
+    people.sort((a, b) => {
+        if (a[1] === b[1]) {
+            return a[0] - b[0]
+        } else {
+            return a[1] - b[1]
+        }
+    });
+    // return people;
+    let temp = people
+    let i = 1;
+    let count = 0;
+    while (i < people.length) {
+        // if (temp[i][1] != 0) {
+        let checkArr = temp.slice(0, i);
+        // c(checkArr);
+        for (let j in checkArr) {
+            checkArr[j][0] >= temp[i][0] ? count++ : '';
+            if (count > temp[i][1]) {
+                // c(j)
+                // c("count " + count)
+                // c("temp[i][0] :" + temp[i][0])
+                // c(temp[j])
+                // c(temp.slice(j - 1, j))
+                let tempI = temp[i];
+
+                temp[i] = temp[j]
+                temp[j] = tempI;
+                i = 0
+                break;
+            }
+            // c(checkArr);
+        }
+        count = 0;
+
         // }
         i++;
     }
