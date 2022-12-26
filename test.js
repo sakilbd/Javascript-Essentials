ad623b55aa069871d1aef28bc8b0d85bde54c64a
sakilbd <mdsakiluzzaman@gmail.com>
2022-12-26 18:03:04 +0600

    some changes

---
+++ test.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/invalid-transactions/
+
+const c = console.log.bind(console);
+
+var invalidTransactions = function(transactions) {
+    return transactions;
+};
+
+
+c(invalidTransactions(["alice,20,800,mtv", "alice,50,1200,mtv"]))
