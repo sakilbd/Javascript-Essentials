98c73196560d53a46da33702394467586eb2f377
sakilbd <mdsakiluzzaman@gmail.com>
2022-12-26 16:16:25 +0600

    maximum-number-of-coins-you-can-get

---
+++ __E__maximum-number-of-coins-you-can-get.js
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/maximum-number-of-coins-you-can-get/description/
+
+const c = console.log.bind(console);
+
+var maxCoins = function(piles) {
+    piles.sort((a, b) => -a + b);
+    let LastArrayLength = piles.length / 3;
+    let rightArray = piles.slice(-LastArrayLength);
+    let leftArray = piles.slice(0, piles.length - LastArrayLength);
+    // return leftArray
+    let modifiedArr = [];
+    let j = 0;
+    // c(piles)
+    // c(leftArray)
+    for (let i = 0; i < leftArray.length; i++) {
+        if ((i + 1) % 2 == 0) {
+            modifiedArr.push(leftArray[i]);
+            modifiedArr.push(rightArray[j]);
+
+            j++;
+        } else {
+            modifiedArr.push(leftArray[i]);
+        }
+    }
+    // return modifiedArr
+    let i = 1;
+    let res = 0;
+    // c(piles);
+    while (modifiedArr[i]) {
+        res += modifiedArr[i];
+        i += 3;
+    }
+    return res;
+};
+
+// c(maxCoins([2, 4, 1, 2, 7, 8]))
+c(maxCoins([9, 8, 7, 6, 5, 1, 2, 3, 4]));
