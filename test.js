2377d86a9d4c36e4bf9843bba072049ddb4648bb
sakilbd <mdsakiluzzaman@gmail.com>
2022-12-27 13:52:15 +0600

    some changes

--- test.js
+++ test.js
@@ -3,8 +3,19 @@
 const c = console.log.bind(console);
 
 var invalidTransactions = function(transactions) {
-    return transactions;
+    let map = {};
+    let res = [];
+    transactions.forEach((item) => {
+        item.split(',')[2] > 1000 ? res.push(item) : '';
+        let name = item.split(",")[0];
+        if (map[name]) {
+            c(map[name])
+        }
+        // !map[name] ? map[name] = item.split(',').slice(1, 4) : '';
+        map[name] = item.split(',').slice(1, 4);
+
+    });
+    return map;
 };
 
-
-c(invalidTransactions(["alice,20,800,mtv", "alice,50,1200,mtv"]))
+c(invalidTransactions(["alice,20,800,mtv", "alice,50,1200,mtv"]));
