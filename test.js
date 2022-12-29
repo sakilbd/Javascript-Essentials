bf24d3db2d2af2e5e7801e29d852cb916d48de98
sakilbd <mdsakiluzzaman@gmail.com>
2022-12-29 17:35:41 +0600

    some changes

--- test.js
+++ test.js
@@ -1,1 +1,35 @@
 const c = console.log.bind(console);
+
+var deleteAndEarn = function(nums) {
+    let max = Math.max(...nums);
+    let points = [];
+    let next = points - 1;
+    let temp = [...nums];
+    let set = [...new Set(nums)];
+    let tempPoint = 0;
+    // return temp
+    // temp = temp.filter(item => item != 3 - 1 && item != 3 + 1)
+    // return temp
+    set.forEach((item) => {
+
+        while (temp.length != 0) {
+            let index = temp.indexOf(item);
+            tempPoint += item;
+            temp.splice(index, 1);
+            temp = temp.filter(elem => elem != item - 1 && elem != item + 1)
+            c(temp);
+
+
+        }
+        temp = [...nums];
+        // c(temp)
+        points.push(tempPoint);
+        tempPoint = 0;
+
+
+    });
+
+    return points;
+};
+
+c(deleteAndEarn([3, 4, 2]));
