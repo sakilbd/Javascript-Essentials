1e9cd5baba211db575969569d28a09a5e667b642
sakilbd <mdsakiluzzaman@gmail.com>
2023-01-01 12:42:10 +0600

    some changes

--- test.js
+++ test.js
@@ -1,21 +1,17 @@
+// https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/description/
 const c = console.log.bind(console);
 
 var reverseParentheses = function(s) {
-
-    let _try = s.split('()');
+    let _try = s.split("()");
     while (_try.length > 1) {
-        s = _try.join('')
-        _try = s.split('()')
+        s = _try.join("");
+        _try = s.split("()");
     }
-
 
     s = _try[0];
 
-
-
-
     let start = s.lastIndexOf("(");
-    let end = start + (s.slice(start, s.length)).indexOf(')');
+    let end = start + s.slice(start, s.length).indexOf(")"); //`start` added as new index will be generated based on slice
     // let end = s.indexOf(")");
 
     // return `${start}+${end}`
@@ -23,28 +19,26 @@ var reverseParentheses = function(s) {
     let slice = temp.slice(start + 1, end);
     // return slice
     while (start != -1) {
-
         let left = temp.slice(0, start);
         let right = temp.slice(end + 1, s.length);
-        c(temp)
 
         temp = left + slice.split("").reverse().join("") + right;
         // return temp
         start = temp.lastIndexOf("(");
-        end = temp.indexOf(")");
+        end = start + temp.slice(start, temp.length).indexOf(")");
         slice = temp.slice(start + 1, end);
-
+        // c(slice)
+        // return `${start}+${end}+${slice}`
         // return slice.split('').reverse().join('')
     }
     return temp;
     // return (temp.slice(1, temp.length - 1)).split('').reverse().join('')
 };
 
-// c(reverseParentheses("(ed(et(oc))el)"));
+c(reverseParentheses("(ed(et(oc))el)"));
 
-// c(reverseParentheses("a(bcdefghijkl(mno)p)q"));
+c(reverseParentheses("a(bcdefghijkl(mno)p)q"));
 
-// c(reverseParentheses("ta()usw((((ab))))"));
+c(reverseParentheses("ta()usw((((ab))))"));
 
-
-c(reverseParentheses("sxmdll(q)eki(x)"))
+c(reverseParentheses("sxmdll(q)eki(x)"));
