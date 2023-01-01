57a4123d828c6e5bd20503419daa269dbbad38d2
sakilbd <mdsakiluzzaman@gmail.com>
2023-01-01 13:23:44 +0600

    some changes

---
+++ test.js
@@ -0,0 +1,31 @@
+const c = console.log.bind(console);
+
+var simplifyPath = function(path) {
+    let length = 0;
+    let stack = [];
+    while (length < path.length) {
+        let slash = stack.pop();
+
+        if (
+            (slash == "/" && path[length] == "/") ||
+            (path[length] == "/" && length == path.length - 1)
+        ) {
+            stack.push(slash);
+        } else {
+            slash ? stack.push(slash) : "";
+            if (path[length] != ".") {
+
+                stack.push(path[length]);
+            }
+        }
+        length++;
+    }
+
+    return stack.join("");
+};
+
+c(simplifyPath("/home//foo/"));
+
+c(simplifyPath("/home/"));
+
+c(simplifyPath("/../"));
