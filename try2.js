d3fad613c41748ab60dff46b10b075e77e964418
sakilbd <mdsakiluzzaman@gmail.com>
2023-01-08 12:55:10 +0600

    some updates

---
+++ try2.js
@@ -0,0 +1,16 @@
+const c = console.log.bind(console);
+
+
+var maxPoints = function(points) {
+    return points;
+};
+
+
+c(maxPoints([
+    [1, 1],
+    [3, 2],
+    [5, 3],
+    [4, 1],
+    [2, 3],
+    [1, 4]
+]))
