5ed10ebd44dceec74f412bc3f158fde1b4036300
sakilbd <mdsakiluzzaman@gmail.com>
2023-01-08 13:43:55 +0600

    some updates

--- try2.js
+++ try2.js
@@ -2,6 +2,21 @@ const c = console.log.bind(console);
 
 
 var maxPoints = function(points) {
+    let map = new Map();
+    let linePoints = [];
+    const collinear = (x1, x2, x3, y1, y2, y3) => {
+        return (y1 - y2) * (x1 - x3) == (y1 - y3) * (x1 - x2);
+    }
+    let i = 0;
+    while (i < points.length) {
+        map.set(points[i], points[i + 1])
+
+        i++
+        // return false;
+    }
+
+
+    return map;
     return points;
 };
 
