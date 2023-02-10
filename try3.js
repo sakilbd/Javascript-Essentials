29d7755ff1173beb1bd15b6ef16dcf2fa84a7653
unknown <mdsakiluzzaman@gmail.com>
2023-02-10 19:22:07 +0600

    merge

--- try3.js
+++ try3.js
@@ -1,20 +1,3 @@
 const c = console.log.bind(console)
 
-const numberOnly = (a, b, c) => {
-    // boom boom way :/
-    if (typeof a != 'number' || typeof b != 'number' || typeof c != 'number') {
-        return "all parameters should be number"
-    }
-    //smart way 1
-    if (!([a, b, c].every(item => item == 'number'))) {
-        return "all parameters should be number"
-    }
-    //smart way 2
-    if ([a, b, c].some(item => item != 'number')) {
-        return "all parameters should be number"
-    }
-    return "all parameters are number"
-}
-
-
-c(numberOnly(1, 2, "2"))
+console.log("123" + 123);
