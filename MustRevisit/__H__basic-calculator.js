628e30e8cb23c6b97b12203719228f10f24c8ba3
sakilbd <mdsakiluzzaman@gmail.com>
2023-02-14 13:57:13 +0600

    basic-calculator

--- MustRevisit/__H__basic-calculator.js
+++ MustRevisit/__H__basic-calculator.js
@@ -1,60 +1,60 @@
-// https://leetcode.com/problems/basic-calculator-ii/description/
-
+// https://leetcode.com/problems/basic-calculator/description/
 const c = console.log.bind(console);
 
-// var calculate = function(s) {
-//     if (s == 0) {
-//         return 0;
-//     }
-//     let numStack = [];
-//     let secondStack = [];
-//     const length = s.length;
-//     let num = "";
-//     for (let i = length - 1; i >= 0; i--) {
-//         if (s[i] == "0") {
-//             num += 0;
-//             i == 0 ? numStack.push(parseInt(num.split("").reverse().join(""))) : "";
-//         } else {
-//             if (parseInt(s[i])) {
-//                 num += s[i];
-//                 i == 0 ? numStack.push(parseInt(num.split("").reverse().join(""))) : "";
-//             } else {
-//                 if (s[i] != " ") {
-//                     if (num.length > 0) {
-//                         numStack.push(parseInt(num.split("").reverse().join("")));
-//                         num = "";
-//                     }
+// this one works as .eval used but should not be used 
+var calculate = function(s) {
+    if (s == 0) {
+        return 0;
+    }
+    let numStack = [];
+    let secondStack = [];
+    const length = s.length;
+    let num = "";
+    for (let i = length - 1; i >= 0; i--) {
+        if (s[i] == "0") {
+            num += 0;
+            i == 0 ? numStack.push(parseInt(num.split("").reverse().join(""))) : "";
+        } else {
+            if (parseInt(s[i])) {
+                num += s[i];
+                i == 0 ? numStack.push(parseInt(num.split("").reverse().join(""))) : "";
+            } else {
+                if (s[i] != " ") {
+                    if (num.length > 0) {
+                        numStack.push(parseInt(num.split("").reverse().join("")));
+                        num = "";
+                    }
 
-//                     numStack.push(parseInt(s[i]) ? parseInt(s[i]) : s[i]);
-//                 } else if (s[i] == " ") {
-//                     if (num.length > 0) {
-//                         numStack.push(parseInt(num.split("").reverse().join("")));
-//                         num = "";
-//                     }
-//                 }
-//             }
-//         }
-//     }
-//     // return numStack;
-//     let i = 0;
-//     let numStackLength = numStack.length;
-//     while (i < numStackLength) {
-//         let popped = numStack.pop();
+                    numStack.push(parseInt(s[i]) ? parseInt(s[i]) : s[i]);
+                } else if (s[i] == " ") {
+                    if (num.length > 0) {
+                        numStack.push(parseInt(num.split("").reverse().join("")));
+                        num = "";
+                    }
+                }
+            }
+        }
+    }
+    // return numStack;
+    let i = 0;
+    let numStackLength = numStack.length;
+    while (i < numStackLength) {
+        let popped = numStack.pop();
 
-//         if (popped === "*") {
-//             let calc = Math.floor(eval(numStack.pop() * secondStack.pop()));
-//             secondStack.push(calc);
-//         } else if (popped === "/") {
-//             let calc = Math.floor(eval(secondStack.pop() / numStack.pop()));
+        if (popped === "*") {
+            let calc = Math.floor(eval(numStack.pop() * secondStack.pop()));
+            secondStack.push(calc);
+        } else if (popped === "/") {
+            let calc = Math.floor(eval(secondStack.pop() / numStack.pop()));
 
-//             secondStack.push(calc);
-//         } else {
-//             secondStack.push(popped);
-//         }
-//         i++;
-//     }
-//     return eval(secondStack.join(""));
-// };
+            secondStack.push(calc);
+        } else {
+            secondStack.push(popped);
+        }
+        i++;
+    }
+    return eval(secondStack.join(""));
+};
 // c(Math.floor(eval(0 / 3)))
 
 // c(parseInt(q.split('').reverse().join('')))
@@ -71,6 +71,8 @@ const c = console.log.bind(console);
 // c(calculate("0"));
 // c(calculate("0*0"));
 
+
+// this one has some progress wihout .eval  but last test case failed 
 const calculate = (s) => {
     // return eval(s)
     if (s == 0) {
@@ -127,11 +129,11 @@ const calculate = (s) => {
     for (let i = 0; i < length; i++) {
         if (s[i] == "0") {
             num += 0;
-            i == 0 ? numStack.push(parseInt(num.split("").join(""))) : "";
+            i == length - 1 ? numStack.push(parseInt(num.split("").join(""))) : "";
         } else {
             if (parseInt(s[i])) {
                 num += s[i];
-                i == 0 ? numStack.push(parseInt(num.split("").join(""))) : "";
+                i == length - 1 ? numStack.push(parseInt(num.split("").join(""))) : "";
             } else {
                 if (s[i] != " ") {
                     if (num.length > 0) {
@@ -167,8 +169,8 @@ const calculate = (s) => {
 
 
 
-    return numStack
-        // return plainStringCalculate(numStack)
+    // return numStack
+    // return plainStringCalculate(numStack)
     if (!nonSyombleStringCheck) {
         return plainStringCalculate(numStack)
     }
@@ -208,7 +210,7 @@ const calculate = (s) => {
 
         i++;
     }
-    return plainStringCalculate(secondStack)
+    // return plainStringCalculate(secondStack)
     return secondStack;
     // return secondStack
     // return val;
@@ -223,10 +225,12 @@ const calculate = (s) => {
     return numStack;
 };
 
-// c(calculate("(1+(2-1+2)-3)+(6+80)"));
-// c(calculate(" 2-1 + 21 "));
+c(calculate("(1+(2-1+2)-3)+(6+80)"));
+c(calculate(" 2-1 + 21 "));
 
 
-// c(calculate(" 2-1 + 2 "))
+c(calculate(" 2-1 + 2 "))
 
 c(calculate("1 + 1"))
+
+c(calculate("1-(     -2)"))
