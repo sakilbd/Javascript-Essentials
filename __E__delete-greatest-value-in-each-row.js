4e7bddcdce898a6b44637d7d90902f7da2fef162
sakilbd <mdsakiluzzaman@gmail.com>
2023-04-25 21:14:02 +0600

    delete-greatest-value-in-each-row

--- __E__delete-greatest-value-in-each-row.js
+++ __E__delete-greatest-value-in-each-row.js
@@ -18,5 +18,24 @@ var deleteGreatestValue = function (grid) {
     return result;
 
 };
+//takes less time 
+var deleteGreatestValue = function (grid) {
+   let result = 0;
+   grid.forEach(item=>{
+       item.sort((a,b)=>b-a)
+   })
+   while (grid[0].length != 0) {
+       let maxValueArray = []
+       for (let item of grid) {
+          
+           maxValueArray.push(item.pop());
+       }
+       // c(maxValueArray)
+       result += Math.max(...maxValueArray)
+       maxValueArray = [];
+   }
+   return result;
+
+};
 
 c(deleteGreatestValue([[1, 2, 4], [3, 3, 1]]))
